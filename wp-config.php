34bd115461e55461a35bbb245f93a9bd28508cbf
alexus693 <alexusndawula@gmail.com>
2020-08-24 12:43:00 +0300

    my first website

---
+++ wp-config.php
@@ -0,0 +1,125 @@
+<?php
+/**
+ * The base configuration for WordPress
+ *
+ * The wp-config.php creation script uses this file during the
+ * installation. You don't have to use the web site, you can
+ * copy this file to "wp-config.php" and fill in the values.
+ *
+ * This file contains the following configurations:
+ *
+ * * MySQL settings
+ * * Secret keys
+ * * Database table prefix
+ * * ABSPATH
+ *
+ * @link https://wordpress.org/support/article/editing-wp-config-php/
+ *
+ * @package WordPress
+ */
+
+// ** MySQL settings - You can get this info from your web host ** //
+/** The name of the database for WordPress */
+define( 'DB_NAME', 'bitnami_wordpress' );
+
+/** MySQL database username */
+define( 'DB_USER', 'bn_wordpress' );
+
+/** MySQL database password */
+define( 'DB_PASSWORD', 'a56bba32b1' );
+
+/** MySQL hostname */
+define( 'DB_HOST', '127.0.0.1:3306' );
+
+/** Database Charset to use in creating database tables. */
+define( 'DB_CHARSET', 'utf8' );
+
+/** The Database Collate type. Don't change this if in doubt. */
+define( 'DB_COLLATE', '' );
+
+/**#@+
+ * Authentication Unique Keys and Salts.
+ *
+ * Change these to different unique phrases!
+ * You can generate these using the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
+ * You can change these at any point in time to invalidate all existing cookies. This will force all users to have to log in again.
+ *
+ * @since 2.6.0
+ */
+define('AUTH_KEY',         '16c4b7e8ec5744930fb29f9a790d625f31745673a5dac4ad7a1d7061a78fe8c1');
+define('SECURE_AUTH_KEY',  '09c7da563cc65cf27546cd580ee1a54b6e5569b8f887b4fe1ef7a4d6d9ba4827');
+define('LOGGED_IN_KEY',    '4f0b5957edc9c15d35ce0bd56149872103f8581044e9e77e62f70d01e3a5db8f');
+define('NONCE_KEY',        '6131cb3555a66623b9f1558bef6d76337d0d0d4cb6eb7b4a41d318431d45f692');
+define('AUTH_SALT',        'd538d4b6ca9266039669b6ee6fd7cfd694a9cc257b1845822e8cca10c3bc3862');
+define('SECURE_AUTH_SALT', '0805e63dab3dfd6bae835e1611df4f9074dcf46dde75bf2441e723a1198a8bf7');
+define('LOGGED_IN_SALT',   'e5f865e5160acaa4d6a75bb9a86a03ce0eadd2f0ecfe2e4c2b05d24f24a6c671');
+define('NONCE_SALT',       'a234505c1faeaf784b8b09b5b6ed6c3372b00d4268f8a5b6b91b34915ffca554');
+
+/**#@-*/
+
+/**
+ * WordPress Database Table prefix.
+ *
+ * You can have multiple installations in one database if you give each
+ * a unique prefix. Only numbers, letters, and underscores please!
+ */
+$table_prefix = 'wp_';
+
+/**
+ * For developers: WordPress debugging mode.
+ *
+ * Change this to true to enable the display of notices during development.
+ * It is strongly recommended that plugin and theme developers use WP_DEBUG
+ * in their development environments.
+ *
+ * For information on other constants that can be used for debugging,
+ * visit the documentation.
+ *
+ * @link https://wordpress.org/support/article/debugging-in-wordpress/
+ */
+define( 'WP_DEBUG', false );
+
+/* That's all, stop editing! Happy publishing. */
+/**
+ * The WP_SITEURL and WP_HOME options are configured to access from any hostname or IP address.
+ * If you want to access only from an specific domain, you can modify them. For example:
+ *  define('WP_HOME','http://example.com');
+ *  define('WP_SITEURL','http://example.com');
+ *
+*/
+
+if ( defined( 'WP_CLI' ) ) {
+    $_SERVER['HTTP_HOST'] = 'localhost';
+}
+
+define('WP_SITEURL', 'http://' . $_SERVER['HTTP_HOST'] . '/wordpress');
+define('WP_HOME', 'http://' . $_SERVER['HTTP_HOST'] . '/wordpress');
+
+
+/** Absolute path to the WordPress directory. */
+if ( ! defined( 'ABSPATH' ) ) {
+	define( 'ABSPATH', __DIR__ . '/' );
+}
+
+/** Sets up WordPress vars and included files. */
+require_once ABSPATH . 'wp-settings.php';
+
+define('WP_TEMP_DIR', 'C:\Bitnami\wordpress-5.4.1-0/apps/wordpress/tmp');
+
+
+//  Disable pingback.ping xmlrpc method to prevent Wordpress from participating in DDoS attacks
+//  More info at: https://docs.bitnami.com/general/apps/wordpress/troubleshooting/xmlrpc-and-pingback/
+
+if ( !defined( 'WP_CLI' ) ) {
+    // remove x-pingback HTTP header
+    add_filter('wp_headers', function($headers) {
+        unset($headers['X-Pingback']);
+        return $headers;
+    });
+    // disable pingbacks
+    add_filter( 'xmlrpc_methods', function( $methods ) {
+            unset( $methods['pingback.ping'] );
+            return $methods;
+    });
+    add_filter( 'auto_update_translation', '__return_false' );
+}
---
+++ wp-content/themes/coffeecafe/readme.txt
@@ -0,0 +1,47 @@
+=== Coffeecafe ===
+
+Contributors: themehunk
+Tags: two-columns, right-sidebar, custom-menu, editor-style, featured-images, translation-ready, flexible-header, post-formats, sticky-post, theme-options
+Requires at least: 4.0.5
+Tested up to: 5.1.1
+Stable tag: 1.0.9
+License: GPLv3 or later
+License URI: https://www.gnu.org/licenses/gpl-3.0.en.html
+
+
+"Coffeecafe One Page Business WordPress Theme" By ThemeHunk.
+
+== Description ==
+The coffeecafe One Page Responsive Theme for WordPress is simple and beautiful theme with live customizer. Edit and check changes instantly. coffeecafe Theme supports Six widget areas (two in the sidebar, four in the footer) and featured images (thumbnails for gallery, blog posts). It has multiple useful section (services, testimonial, WooCommerce, pricing, recent post, team, contact us). Contact section with lead capture functionality. This free theme is very helpful for entrepreneur to showcase their business features. Also this single page theme is very helpful for photography, business, coffee cafe, shopping and personal site. Drag and drop section scrolling and woo-commerce compatible.
+
+== Frequently Asked Questions ==
+
+= Does your theme works with latest WordPress version. =
+Yes, our theme works with latest WordPress version.
+
+= Does your theme support third party plugin =
+Yes, we have created our theme in such a way that it can support almost all plugin available in a market. However, It is very difficult to test each and every plugin. So if you get any issues either you can contact to our support forum. They will help you in every possible manner to make plugin compatible with the theme. Or you can use any alternate plugin.
+
+= Does your theme works on multisite. =
+Yes, all our theme works on multisite.
+
+== Resources ==
+
+License for images:
+1. Image Name: main.jpeg 
+Resource link: https://www.pexels.com/photo/coffee-woman-people-tea-91947/
+Licensed under the CCO license.
+License link : https://www.pexels.com/photo-license/
+
+== Changelog ==
+= Version: 1.0.9=
+* compatible with latest wordpress.
+
+== Upgrade Notice ==
+* compatible with latest wordpress.
+
+== Theme License & Copyright ==
+Coffeecafe is distributed under the terms of the GNU GPL
+Coffeecafe -Copyright 2016 Coffeecafe, ThemeHunk.com
+Once again, thank you so much for trying the Coffeecafe WordPress Theme. As we said at the beginning, we will be glad to help you. If you have any questions related to this theme then do let us know & we will try our best to assist. If you have any general questions related to the themes on ThemeHunk, we would recommend you to visit the ThemeHunk Support Forum and ask your queries there.
+
